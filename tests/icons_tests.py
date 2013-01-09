9a53268a562f7731d6e7ced4e716af3bc510724a
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-01-09 12:14:44 +1200

    [Add] New test for colors checking in icons. [Change] Rename SVG class to SvgReader. [Add] Method in SvgReader for get colors.

--- tests/icons_tests.py
+++ tests/icons_tests.py
@@ -3,7 +3,7 @@ __author__ = 'bryukh'
 import unittest
 import os
 import icons_settings as settings
-from svg import SVG
+from svg import SvgReader
 
 PATH = '../icons_tasks/'
 ALT_PATH = './icons_tasks'
@@ -16,9 +16,10 @@ def suite():
     test_suite = unittest.TestSuite()
     tests_names = unittest.TestLoader().getTestCaseNames(IconTest)
     for filename in svgfiles:
-        svg = SVG(filename)
+        svg = SvgReader(filename)
         test_suite.addTests([IconTest(svg, filename, methodName=test_name) for test_name in tests_names])
     return test_suite
+
 
 class IconTest(unittest.TestCase):
     def __init__(self, svg, filename, *args, **kwargs):
@@ -28,13 +29,20 @@ class IconTest(unittest.TestCase):
 
     def test_size(self):
         size = self.svg.get_size()
-        self.assertEqual(size, settings.SIZE, "Wrong size {0}x{1} for {2}".format(size[0], size[1], self.filename))
+        self.assertEqual(size, settings.SIZE, "Wrong size {0}x{1} for file {2}".format(size[0], size[1], self.filename))
 
     def test_opacity_background(self):
         for elem in self.svg.get_backgrounds():
             for child_elem in elem.getchildren():
                 opacity = child_elem.get('fill-opacity')
-                self.assertEqual(opacity, '0', "Wrong opacity {0} for {1}".format(opacity, self.filename))
+                self.assertEqual(opacity, '0', "Wrong opacity {0} for file {1}".format(opacity, self.filename))
+
+    def test_colors(self):
+        elements_colors = self.svg.get_elements_colors()
+        for el in elements_colors:
+            self.assertIn(el['color'], settings.COLORS,
+                'Wrong color {0} {1} in tag {2} id {3} for file {4}'.format(el['color'],
+                    el['type'], el['tag'], el['id'], self.filename))
 
 if __name__ == '__main__':
     unittest.TextTestRunner(verbosity=2).run(suite())
--- tests/svg.py
+++ tests/svg.py
@@ -1,16 +1,31 @@
 __author__ = 'bryukh'
 from xml.etree import ElementTree
+import re
 
 
-class SVG(object):
+class SvgReader(object):
     def __init__(self, source):
         with open(source) as file:
             parsed_svg = ElementTree.parse(file)
         self.root = parsed_svg.getroot()
         self.elements = self.root.iter()
+        temp_re = re.match("^{.*?}", self.root.tag)
+        self.xlmns = temp_re.group() if temp_re else {}
 
     def get_size(self):
         return int(self.root.attrib['width']), int(self.root.attrib['height'])
 
     def get_backgrounds(self):
         return (el for el in self.elements if el.attrib.get('id', '') == 'Background')
+
+    def get_elements_colors(self):
+        res = []
+        colors_attrib = [('fill', 'fill-opacity'), ('stroke', 'stroke-opacity')]
+        for el in self.elements:
+            for attr in colors_attrib:
+                if el.attrib.get(attr[1], '100') != '0' and el.attrib.get(attr[0]):
+                    res.append({'color': el.attrib[attr[0]],
+                                'type': attr[0],
+                                'tag': el.tag.replace(self.xlmns, ''),
+                                'id': el.attrib.get('id', '')})
+        return res
